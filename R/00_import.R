60bfd86635be1c67dfaa006f2ac8758e284d954a
Aditya <aditya.bhagwat@mpi-bn.mpg.de>
2020-06-29 11:05:01 +0200

    Update imports.R, NAMESPACE, man

--- R/00_import.R
+++ R/00_import.R
@@ -1,15 +1,24 @@
 #' @importFrom AnnotationDbi mapIds
 #' @importFrom affy          just.rma
-#' @importFrom assertive     assert_all_are_existing_files   assert_is_a_bool
+#' @importFrom assertive     assert_engine   assert_all_are_existing_files
+#' @importFrom assertive     assert_all_are_whole_numbers
+#' @importFrom assertive     assert_all_are_in_closed_range
+#' @importFrom assertive     assert_is_all_of   assert_is_a_bool
 #' @importFrom assertive     assert_is_a_number   assert_is_a_string
 #' @importFrom assertive     assert_is_character  assert_is_data.frame
+#' @importFrom assertive     assert_is_not_null
 #' @importFrom assertive     assert_is_numeric    assert_is_subset
-#' @importFrom assertive     is_existing_file
+#' @importFrom assertive     false   get_name_in_parent
 #' @importFrom assertive     has_no_duplicates
+#' @importFrom assertive     is_empty_character   is_existing_file
 #' @importFrom data.table    fread   fwrite   data.table   setkeyv
+#' @importFrom edgeR         filterByExpr
+#' @importFrom graphics      title   lines
+#' @importFrom limma         duplicateCorrelation   lmFit   voom
 #' @importFrom magrittr      %>%   %<>%   add   and   equals
 #' @importFrom magrittr      extract   extract2   is_in
-#' @importFrom magrittr      set_colnames   set_names
+#' @importFrom magrittr      set_colnames   set_names   set_rownames
+#' @importFrom matrixStats   rowAnys   colAnys
 #' @importFrom methods       as
 #' @importFrom parallel      detectCores
 #' @importFrom purrr         detect_index
@@ -17,6 +26,8 @@
 #' @importFrom Rsubread      featureCounts
 #' @importFrom R.utils       gunzip
 #' @importFrom S4Vectors     DataFrame   metadata   metadata<-
+#' @importFrom stats         aggregate    approxfun   as.formula   lowess
+#' @importFrom stats         model.matrix
 #' @import     stringi
 #' @importFrom tools         file_ext   file_path_sans_ext
 #' @importFrom SummarizedExperiment  assays   assays<-   SummarizedExperiment
