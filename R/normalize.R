71ddd4365bd7ce9b01ca3c368e8f2100d6899083
Aditya <aditya.bhagwat@mpi-bn.mpg.de>
2020-10-06 17:34:51 +0200

    Complete transformations (and accompanying plotting)

--- R/normalize.R
+++ R/normalize.R
@@ -1,22 +1,24 @@
 #==============================================================================
 #
-#                                invnorm()
+#                           .zscore()
+#                           .quantnorm()
+#                           .invnorm()
 #
 #==============================================================================
 
+.zscore <- function(object){
+    exprs(object) %<>% scale()
+    object
+}
 
-#' Inverse normal transform samples
-#' @param object SummarizedExperiment
-#' @param plot  TRUE (default) or FALSE
-#' @return normalized SummarizedExperimen
-#' @examples
-#' file <- download_data('glutaminase.metabolon.xlsx')
-#' object <- read_metabolon(file)
-#' invnorm(object)
-#' @export
-invnorm <- function(object, plot = TRUE){
+.quantnorm <- function(object){
+    exprs(object) %<>% limma::normalizeBetweenArrays()
+    object
+}
+
+
+.invnorm <- function(object){
     exprs(object) %<>% apply(2, transform_to_fitting_normal)
-    if (plot)  plot_sample_scores(object, 'pca') + ggtitle('invnorm')
     object
 }
 
@@ -62,6 +64,89 @@ transform_to_standard_normal <- function(x){
 }
 
 
+#==============================================================================
+#
+#                       plot_transformation_densities()
+#
+#==============================================================================
 
+#' Plot transformation densities
+#'
+#' Plot sample densities for alternative transformations
+#'
+#' @param object          SummarizedExperiment
+#' @param geom            function
+#' @param x               svar mapped to x
+#' @param group           svar mapped to group
+#' @param ...             additional svar to aesthetic mappings
+#' @param tranformations  character vector
+#' @param fixed           list
+#' @examples
+#' file <- download_data('glutaminase.metabolon.xlsx')
+#' object <- read_metabolon(file, plot = FALSE)
+#' plot_transformation_densities(object)
+#' @export
+plot_transformation_densities <- function(
+    object,
+    transformations = c('.quantnorm', '.zscore', '.invnorm'),
+    geom = geom_violin, x = subgroup, y = value,
+    group = if (ncol(object) > 16)  subgroup  else  sample_id, ...,
+    fixed = list(na.rm=TRUE)
+){
+    dt <- sumexp_to_long_dt(object)
+    dt$transfo <- 'input'
+    for (transfo in transformations){
+        object <- get(transfo)(object)
+        dt1 <- sumexp_to_long_dt(object)
+        dt1$transfo <- transfo
+        dt %<>% rbind(dt1)
+    }
+    dt$transfo %<>% factor(c('input', transformations))
+    plot_data(dt, geom, x = !!enquo(x), y = !!enquo(y), group = !!enquo(group), color = NULL,
+              fill = subgroup, ..., fixed = fixed) +
+    facet_grid(subgroup~transfo, scales = "free") +
+    coord_flip()
+}
+
+
+#==============================================================================
+#
+#                       invnorm
+#
+#==============================================================================
+
+
+#' Inverse normal transform samples
+#' @param object SummarizedExperiment
+#' @param plot  TRUE (default) or FALSE
+#' @param ...   additional aesthetic mappings
+#' @return normalized SummarizedExperimen
+#' @examples
+#' file <- download_data('glutaminase.metabolon.xlsx')
+#' object <- read_metabolon(file, plot = FALSE)
+#' invnorm(object)
+#' quantnorm(object)
+#' zscore(object)
+#' @export
+invnorm <- function(object, plot = TRUE, ...){
+    if (plot) print(plot_transformation_densities(object, '.invnorm', ...))
+    .invnorm(object)
+}
+
+
+#' @rdname invnorm
+#' @export
+quantnorm <- function(object, plot = TRUE, ...){
+    if (plot) print(plot_transformation_densities(object, '.quantnorm', ...))
+    .quantnorm(object)
+}
+
+
+#' @rdname invnorm
+#' @export
+zscore <- function(object, plot = TRUE, ...){
+    if (plot) print(plot_transformation_densities(object, '.zscore', ...))
+    .zscore(object)
+}
 
 
