932d44c6af39a20a0509b27f63d8104020328888
Johannes Graumann <johannes.graumann@mpi-bn.mpg.de>
2021-02-22 12:57:09 +0000

    Minor fix - handing on 'method' in the context of `biplot_covariates`;

--- R/4_pca.R
+++ R/4_pca.R
@@ -639,7 +639,7 @@ biplot_covariates <- function(
     dimcols = 1, varcols = length(covariates), plot = TRUE
 ){
     x <- y <- NULL
-    plotdt <- prep_covariates(object, method = 'pca', ndim=ndim)
+    plotdt <- prep_covariates(object, method = method, ndim=ndim)
     plotlist <- list()
     for (covar in covariates){
         p <- plot_data(plotdt, geom = geom_point, x=x, y=y, color=!!sym(covar),
