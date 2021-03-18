0dda7b171efca241ade58b487a71866b9a6bb185
Aditya Bhagwat <bhagwataditya@gmail.com>
2021-03-18 15:30:15 +0100

    Fixes

--- R/1_utilities.R
+++ R/1_utilities.R
@@ -1,19 +1,19 @@
 
-#' Conveniently message dataframe
+#' message dataframe
 #'
-#' Conveniently message dataframe using sprintf syntax.
+#' message dataframe using sprintf syntax.
 #' Use place holder '%s' for data.frame.
 #'
 #' @param format_string sprintf style format string
 #' @param x data.frame
-#' @return NULL
+#' @return nothing returned
 #' @examples
 #' x <- data.frame(feature_id = c('F001', 'F002'), symbol = c('FEAT1', 'FEAT2'))
 #' message_df('\t%s', x)
 #'
 #' x <- c(rep('PASS', 25), rep('FAIL', 25))
 #' message_df(format_string = '%s', table(x))
-#' @noRd
+#' @export
 message_df <- function(format_string, x){
     format_string %>%
     sprintf(capture.output(print(x))) %>%
