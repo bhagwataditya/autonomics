245d5a6e71263d6a05dc4b0842ce3067c161e56e
Aditya Bhagwat <aditya.bhagwat@uni-marburg.de>
2022-04-05 13:35:26 +0200

    Pass `feature_id` and `sample_id` to plot_summary()

--- R/4_analyze.R
+++ R/4_analyze.R
@@ -11,6 +11,9 @@
 #' @param contrastdefs NULL or character vector: coefficient contrasts to test
 #' @param verbose      whether to msg
 #' @param plot         whether to plot
+#' @param feature_id   string: which feature to visualize
+#' @param sample_id    string: which sample to visualize
+#' @param palette      color vector: values = colors, names = slevels
 #' @return SummarizedExperiment
 #' @examples 
 #' require(magrittr)
@@ -20,16 +23,19 @@
 #' @export
 analyze <- function(
         object,
-        pca = TRUE,
-        fit = 'limma',
-        subgroupvar = default_subgroupvar(object),
-        formula = default_formula(object, subgroupvar, fit),
-        block = NULL,
-        weightvar = if ('weights' %in% assayNames(object)) 'weights' else NULL,
-        coefs    = colnames(create_design(object, formula = formula)),
+        pca          = TRUE,
+        fit          = 'limma',
+        subgroupvar  = default_subgroupvar(object),
+        formula      = default_formula(object, subgroupvar, fit),
+        block        = NULL,
+        weightvar    = if ('weights' %in% assayNames(object)) 'weights' else NULL,
+        coefs        = colnames(create_design(object, formula = formula)),
         contrastdefs = contrast_coefs(object, formula),
-        verbose = TRUE,
-        plot    = pca & !is.null(fit)
+        verbose      = TRUE,
+        plot         = pca & !is.null(fit),
+        feature_id   = NULL,
+        sample_id    = NULL, 
+        palette      = NULL
 ){
     # Analyze
     if (is.null(subgroupvar))  subgroupvar <- default_subgroupvar(object)
@@ -50,33 +56,69 @@ analyze <- function(
                             plot         = FALSE) 
     }
     # Plot/Return
-    if (plot)  plot_summary(object, subgroupvar, fit)
+    if (plot)  plot_summary(
+        object, subgroupvar = subgroupvar, fit = fit, 
+        feature_id = feature_id, sample_id = sample_id, palette = palette)
     object
 }
 
 
 #' Plot summary
-#' @param object SummarizedExperiment
+#' @param object       SummarizedExperiment
+#' @param subgroupvar  string
+#' @param fit          string
+#' @param coef         string
+#' @param feature_id   NULL or string
+#' @param sample_id    NULL or string
+#' @param palette      NULL or color vector
 #' @examples 
 #' file <- download_data('billing19.rnacounts.txt')
 #' object <- read_rnaseq_counts(file, plot = FALSE)
 #' plot_summary(object, subgroupvar = 'subgroup', fit = 'limma')
 #' @export
-plot_summary <- function(object, subgroupvar, fit){
-    detections   <- plot_top_detections(object, subgroupvar)
-    sampledistr  <- plot_top_sample( object, subgroupvar)
-    featuredistr <- plot_top_feature(object)
-    boxplot      <- plot_top_boxplot(object, subgroupvar)
-    volcano      <- plot_top_volcano(object, fit)
+plot_summary <- function(
+    object, 
+    subgroupvar = 'subgroup',
+    fit         = fits(object)[1], 
+    coef        = c(setdiff(coefs(object), 'Intercept'), 'Intercept')[1],
+    feature_id  = NULL, 
+    sample_id   = NULL, 
+    palette     = NULL
+){
+# Initialize
+    if (is.null(sample_id)){  # most avg sample
+        sample_id  <- snames(object)[which.min(abs(object$pca1))] 
+    }
+    if (is.null(feature_id)){
+        #idx <- which.max(abs(fdata(object)$pca1))
+        pvar <- paste('p', coef, fit, sep = FITSEP)
+        idx <- which.min(fdt(object)[[pvar]])
+        feature_id <- fnames(object)[idx]
+    }
+# Create plots
+    if (is.null(palette))  palette <- make_colors(slevels(object, subgroupvar))
+    detections   <- plot_top_detections(object, subgroupvar = subgroupvar, palette = palette)
+    sampledistr  <- plot_top_sample(object[, sample_id], 
+                                subgroupvar = subgroupvar, palette = palette) + 
+                    ggtitle(assayNames(object)[1]) + 
+                    theme(plot.title = element_text(hjust = 0.5))
+    featuredistr <- plot_top_feature(object[feature_id,])
+    boxplot <- plot_top_boxplot(object[feature_id,], subgroupvar = subgroupvar, 
+                    palette = palette)
+    volcano <- plot_top_volcano(object, fit = fit, coef = coef)
+    # Layout
     layout_matrix <- matrix(c(1,2,5,3,4,5), nrow = 2, byrow = 2)
-    grid.draw(grid.arrange(detections,   sampledistr, 
-                           featuredistr,  boxplot, volcano, 
-                           layout_matrix = layout_matrix))
+    grid.draw(grid.arrange(
+        detections, sampledistr, featuredistr,  boxplot, volcano, 
+        layout_matrix = layout_matrix))
 }
 
-plot_top_detections <- function(object, subgroupvar){
+plot_top_detections <- function(object, subgroupvar, palette = NULL){
     detections <- plot_summarized_detections(
-        object, subgroup = !!sym(subgroupvar), fill = !!sym(subgroupvar))
+                    object, 
+                    subgroup = !!sym(subgroupvar), 
+                    fill     = !!sym(subgroupvar), 
+                    palette  = palette)
     pcaplot <- biplot(object, color = !!sym(subgroupvar), x = pca1, y = pca2) + 
         guides(color = 'none') + ggtitle(NULL) +
         theme(axis.text.x  = element_blank(), 
@@ -89,48 +131,51 @@ plot_top_detections <- function(object, subgroupvar){
         annotation_custom(ggplotGrob(pcaplot), 
                           xmin = 0.2*max(detections$data$xmax), 
                           xmax = 0.8*max(detections$data$xmax), 
-                          ymin = 0.2*max(detections$data$ymax), 
-                          ymax = 0.8*max(detections$data$ymax))
+                          ymin = 0.1*max(detections$data$ymax), 
+                          ymax = 0.7*max(detections$data$ymax))
 }
 
-plot_top_sample <- function(object, subgroupvar){
-    palette <- make_colors(slevels(object, subgroupvar))
-    idx <- which.min(abs(object$pca1))   # most average sample
-    
+plot_top_sample <- function(object, subgroupvar, palette){
     plot_sample_densities(
-        object[, idx], 
+        object, 
         fill = !!sym(subgroupvar), facet = vars(sample_id), palette = palette,
         fixed = list(alpha=1)) + 
-        guides(fill = 'none') + ggtitle(NULL) + coord_flip() + 
+        guides(fill = 'none') + ggtitle(NULL) +
         xlab(NULL) + ylab(NULL) + 
-        theme(axis.text.x = element_blank(), axis.ticks.x = element_blank())
-}
-
-plot_top_feature <- function(object){
-    idx <- which.max(abs(fdata(object)$pca1))
-    
-    plot_feature_densities(
-        object[idx, ],
-        facet = vars(feature_id), fixed = list(fill = 'grey80')) + 
-        ggtitle(NULL) + xlab(NULL) + ylab(NULL) + 
         theme(axis.text.y = element_blank(), axis.ticks.y = element_blank())
 }
 
-plot_top_boxplot <- function(object, subgroupvar){
-    idx <- which.max(abs(fdata(object)$pca1))
-    palette <- make_colors(slevels(object, subgroupvar))
+plot_top_feature <- function(object){
+    plot_feature_densities(
+        object,
+        facet = vars(feature_id), fixed = list(fill = 'grey80')) + 
+        ggtitle(NULL) + xlab(NULL) + ylab(NULL) + 
+        theme(axis.text.x = element_blank(), axis.ticks.x = element_blank()) + 
+        coord_flip()
+}
+
+plot_top_boxplot <- function(object, subgroupvar, palette){
     plot_subgroup_boxplots(
-        object[idx, ], 
-        subgroup = subgroup, fill = subgroup, facet = vars(feature_id),
-        palette = palette) + 
+        object, 
+        subgroup = !!sym(subgroupvar), 
+        fill     = !!sym(subgroupvar), 
+        facet    = vars(feature_id),
+        palette  = palette) + 
         ggtitle(NULL) + guides(fill = 'none') + ylab(NULL)
 }
 
-plot_top_volcano <- function(object, fit){
-    selectedcoef <- summarize_fit(object, fit = fit)
-    selectedcoef %<>% extract(which.max(ndown))  # nup favours intercept !
-    selectedcoef %<>% extract2('contrast')
-    selectedcoef %<>% as.character()
-    plot_volcano(object, coefs = selectedcoef) + 
-        guides(color = 'none') + ggtitle(NULL)
+top_coef <- function(object, fit){
+    summarydt <- summarize_fit(object, fit = fit)
+    summarydt %<>% extract(which.max(ndown))  # nup favours intercept !
+    summarydt$contrast
+}
+
+plot_top_volcano <- function(object, fit, coef){
+    summarydt <- summarize_fit(object, fit = fit)
+    summarydt %<>% extract(contrast==coef)  # nup favours intercept !
+    
+    plot_volcano(object, coefs = coef) + 
+    guides(color = 'none') + 
+    ggtitle(sprintf('%d down | %d up', summarydt$ndown, summarydt$nup)) +
+    theme(plot.title = element_text(hjust = 0.5))
 }
