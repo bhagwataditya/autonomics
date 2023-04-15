e2689f7cb4fed22f26718aa3a4e59576afe92683
Aditya Bhagwat <aditya.bhagwat@uni-marburg.de>
2023-04-15 12:58:17 +0200

    Update `plot_summary` and `analyze`

--- R/4_analyze.R
+++ R/4_analyze.R
@@ -1,31 +1,32 @@
 
 #' Analyze
 #' @param object       SummarizedExperiment
-#' @param pca          TRUE/FALSE: perform pca?
-#' @param fit          model engine: 'limma', 'lm', 'lme(r)', 'lmer', 'wilcoxon'
-#' @param subgroupvar  subgroup svar
+#' @param pca          TRUE / FALSE: perform pca ?
+#' @param pls          TRUE / FALSE: perform pls ?
+#' @param fit          glm engine: 'limma', 'lm', 'lme(r)', 'lmer', 'wilcoxon'
 #' @param formula      model formula
+#' @param drop         TRUE / FALSE : drop varname in designmat ?
+#' @param coding       factor coding system : 'treatment', 'reference', difference', 
+#'                                            'grandref', 'granddiff', 'sum', 'helmert'
+#' @param contrasts    model coefficient contrasts of interest: string vector or NULL
+#' @param coefs        model coefficients          of interest: string vector or NULL
 #' @param block        model blockvar
 #' @param weightvar    NULL or name of weight matrix in assays(object)
-#' @param coefs        model coefficients          of interest: string vector or NULL
-#' @param contrasts    model coefficient contrasts of interest: string vector or NULL
-#' @param plot         TRUE/FALSE
-#' @param feature_id   string: feature for summary plot
-#' @param sample_id    string: sample  for summary plot
-#' @param palette      color palette : named string vector
-#' @param verbose      TRUE/FALSE: message?
+#' @param plot         TRUE / FALSE
+#' @param palette      NULL or colorvector
+#' @param verbose      TRUE / FALSE: message?
 #' @return SummarizedExperiment
 #' @examples 
 #' require(magrittr)
 #' file <- download_data('atkin18.metabolon.xlsx')
 #' object <- read_metabolon(file)
-#' object %<>% analyze(pca = TRUE, fit = 'limma')
+#' object %<>% analyze()
 #' @export
 analyze <- function(
     object,
     pca          = TRUE,
+    pls          = TRUE,
     fit          = 'limma',
-    subgroupvar  = default_subgroupvar(object),
     formula      = default_formula(object),
     drop         = varlevels_dont_clash(object, all.vars(formula)),
     coding       = 'treatment', 
@@ -34,8 +35,6 @@ analyze <- function(
     block        = NULL,
     weightvar    = if ('weights' %in% assayNames(object)) 'weights' else NULL,
     plot         = pca & !is.null(fit),
-    feature_id   = NULL,
-    sample_id    = NULL,
     palette      = NULL,
     verbose      = TRUE
 ){
@@ -43,6 +42,7 @@ analyze <- function(
     if (is.null(subgroupvar))   subgroupvar <- default_subgroupvar(object)
     if (is.null(palette))       palette <- make_subgroup_palette(object)
     if (pca)  object %<>% pca(verbose = verbose, plot = FALSE)
+    if (pls)  object %<>% pls(by = all.vars(formula)[1], verbose = FALSE)
     for (curfit in fit){
         fitfun <- get(paste0('fit_', curfit))
         if (is.null(formula)) formula <- default_formula(object)
@@ -50,100 +50,101 @@ analyze <- function(
         object %<>% fitfun(
             formula      = formula,       drop         = drop,
             coding       = coding,        contrasts    = contrasts,
-            coefs        = coefs,
-            block        = block,         weightvar    = weightvar,
-            verbose      = verbose,       plot         = FALSE) 
+            coefs        = coefs,         block        = block,
+            weightvar    = weightvar,     verbose      = verbose,
+            plot         = FALSE) 
     }
     # Plot/Return
-    if (plot)  plot_summary(object, fit = fit, feature_id = feature_id, 
-                            sample_id = sample_id, palette = palette)
+    if (plot)  plot_summary(object, fit = fit, palette = palette)
     object
 }
 
 
 #' Plot summary
 #' @param object       SummarizedExperiment
-#' @param fit          string
-#' @param coef         string
-#' @param feature_id   NULL or string
-#' @param sample_id    NULL or string
-#' @param palette      NULL or color vector
+#' @param fit          glm engine : 'limma', 'lm', 'lme', 'lmer' or 'wilcoxon'
+#' @param block        NULL or svar
+#' @param palette      NULL or colorvector
 #' @examples 
 #' file <- download_data('atkin18.metabolon.xlsx')
 #' object <- read_metabolon(file)
+#' object %<>% pca()
+#' object %<>% pls(by = 'subgroup')
 #' object %<>% fit_limma()
+#' plot_summary(object, block = 'SUB')
 #' @export
 plot_summary <- function(
-    object, 
-    fit         = fits(object)[1], 
-    coef        = c(setdiff(coefs(object), 'Intercept'), 'Intercept')[1],
-    feature_id  = NULL, 
-    sample_id   = NULL, 
-    palette     = make_subgroup_palette(object)
+    object, fit = 'limma', block = NULL, palette = make_subgroup_palette(object)
 ){
-# Initialize
-    if (is.null(sample_id)){  # most avg sample
-        object %<>% pca()
-        sample_id  <- snames(object)[which.min(abs(object$`effect~samples~pca1`))] 
-    }
-    if (is.null(feature_id)){
-        #idx <- which.max(abs(fdata(object)$pca1))
-        pvar <- paste('p', coef, fit, sep = FITSEP)
-        idx <- which.min(fdt(object)[[pvar]])
-        feature_id <- fnames(object)[idx]
-    }
-# Create plots
-    detections <- plot_subgroup_nas(
-        object, 
-        palette  = palette) + ggtitle('Detections') + xlab('subgroup') + 
-        theme(plot.title = element_text(hjust = 0.5))
-
-    pcaplot <- biplot(
-        object, color = 'subgroup', method = 'pca', colorpalette = palette) + 
-        guides(color = 'none') + ggtitle(NULL) +
-        theme(axis.text.x  = element_blank(), 
-              axis.text.y  = element_blank(), 
-              axis.ticks.x = element_blank(), 
-              axis.ticks.y = element_blank()) +
-        scale_x_continuous(expand = c(0.2, 0.2)) + 
-        ggtitle('Pca') +
-        theme(plot.title = element_text(hjust = 0.5))
-
-    sample  <- plot_top_sample(object[, sample_id],  palette = palette)
-    feature <- plot_exprs(object[feature_id,])
-    volcano <- plot_top_volcano(object, fit = fit, coef = coef)
-    # Layout
-    layout_matrix <- matrix(c(1,2,3,4,5,3), nrow = 2, byrow = TRUE)
-    grid.arrange(pcaplot, detections, volcano, 
-                 sample, feature, layout_matrix = layout_matrix)
+# Assert
+    assert_is_valid_sumexp(object)
+    assert_is_subset(c('effect~sample_id~pca1', 'effect~sample_id~pca2'), svars(object))
+    assert_is_subset(c('effect~sample_id~pca1', 'effect~sample_id~pca2'), fvars(object))
+    assert_is_subset(c('effect~subgroup~pls1',  'effect~subgroup~pls2'), svars(object))
+    assert_is_subset(c('effect~subgroup~pls1',  'effect~subgroup~pls2'), fvars(object))
+    assertive::assert_any_are_matching_regex(fvars(object), 'limma')
+# Plot
+    detections <- plot_subgroup_nas(object, 
+                    palette  = palette) + ggtitle('Detections') + xlab(NULL) + 
+                    theme(plot.title = element_text(hjust = 0.5))
+    pcaplot <- biplot(object, method = 'pca', color = 'subgroup', colorpalette = palette, nfeatures = 1) + 
+               guides(color = 'none', linetype = 'none') + # scale_x_continuous(expand = c(0.2, 0.2)) + 
+               theme(axis.text.x  = element_blank(), axis.text.y  = element_blank(), 
+                     axis.ticks.x = element_blank(), axis.ticks.y = element_blank(), 
+                     plot.title   = element_text(hjust = 0.5))
+    plsplot <- biplot(object, method = 'pls', color = 'subgroup', colorpalette = palette) + 
+               guides(color = 'none', linetype = 'none') + # scale_x_continuous(expand = c(0.2, 0.2)) +
+               theme(axis.text.x  = element_blank(), axis.text.y  = element_blank(), 
+                     axis.ticks.x = element_blank(), axis.ticks.y = element_blank(), 
+                     plot.title   = element_text(hjust = 0.5))
+    samples  <- plot_top_samples(object, palette = palette)
+    features <- plot_top_features(object)
+    pca1exprs <- plot_exprs(object, fit = 'pca1',  block = block, n = 1, subtitle = 'X1', title = NULL) + guides(color = 'none') + ylab(NULL)
+    pca2exprs <- plot_exprs(object, fit = 'pca2',  block = block, n = 1, subtitle = 'X2', title = NULL) + guides(color = 'none') + ylab(NULL)
+    pls1exprs <- plot_exprs(object, fit = 'pls1',  block = block, n = 1, subtitle = 'X1', title = NULL) + guides(color = 'none') + ylab(NULL)
+    pls2exprs <- plot_exprs(object, fit = 'pls2',  block = block, n = 1, subtitle = 'X2', title = NULL) + guides(color = 'none') + ylab(NULL)
+    coefs <- default_coefs(object, fit = fit)
+    glm1exprs <- plot_exprs(object, fit = fit, coefs = coefs[1], block = block, n = 1, nrow = 1, subtitle = coefs[1], title = NULL) + 
+                 guides(color = 'none') + ylab(NULL)
+    glm1volcano <- plot_top_volcano(object, fit = fit, coef = coefs[1]) + guides(shape = 'none', linetype = 'none')
+# Arrange
+    layout_matrix <- matrix(c(1, 2, 3, 
+                              4, 5, 6, 
+                              7, 8, 6, 
+                              9,10,11), nrow = 4, byrow = TRUE)
+    grid.arrange(detections, samples, features, 
+                 pcaplot,   plsplot,   glm1volcano, 
+                 pca1exprs, pls1exprs,#glm1volcano, 
+                 pca2exprs, pls2exprs, glm1exprs,
+                 layout_matrix = layout_matrix)
 }
 
 
-plot_top_sample <- function(object, palette = make_subgroup_palette(object)){
-    plot_sample_densities(
-        object, 
-        fill = 'subgroup', facet = 'sample_id', palette = palette,
-        fixed = list(alpha=1, na.rm = TRUE), labeller = label_value) + 
-        guides(fill = 'none') + ggtitle('Sample') +
-        xlab(assayNames(object)[1]) + ylab(NULL) + 
-        theme(axis.text.y  = element_blank(), 
-              axis.ticks.y = element_blank(), 
-              plot.title = element_text(hjust = 0.5))
+plot_top_samples <- function(object, palette = make_subgroup_palette(object)){
+    plot_sample_densities( object, n = 5, fill = 'subgroup', palette = palette) + 
+        ggtitle('Samples') + theme_void() + coord_flip() + 
+        theme(plot.title       = element_text(hjust = 0.5), 
+              legend.position  = 'left', 
+              legend.direction = 'vertical', 
+              legend.title     = element_blank())
 }
 
-plot_top_feature <- function(
-    object, palette = make_subgroup_palette(object)
-){
-    plot_subgroup_boxplots(
-        object, 
-        subgroup = 'subgroup', 
-        fill     = 'subgroup', 
-        facet    = 'feature_id',
-        palette  = palette, 
-        labeller = label_value) + 
-    ggtitle('Feature') + guides(fill = 'none') + 
-    ylab(assayNames(object)[1]) +
-    theme(plot.title = element_text(hjust = 0.5))
+plot_top_features <- function(object, fit){
+    idx1 <- order(abs(fdt(object)$`effect~sample_id~pca1`), decreasing = TRUE)[1]
+    idx2 <- order(abs(fdt(object)$`effect~sample_id~pca2`), decreasing = TRUE)[1]
+    idx3 <- order(abs(fdt(object)$`effect~subgroup~pls1` ), decreasing = TRUE)[1]
+    idx4 <- order(abs(fdt(object)$`effect~subgroup~pls2` ), decreasing = TRUE)[1]
+    pvr <- pvar(object, fit = fit, coef = default_coefs(object, fit = fit))[1]
+    idx5 <- order(abs(fdt(object)[[pvr]]))[1]
+    idx  <- unique(c(idx1,idx2,idx3,idx4,idx5))
+    
+    plot_feature_densities(
+        object[idx, ], n = length(idx)) + 
+        xlab(feature_id) + ggtitle('Features') + theme_void() + 
+        theme(plot.title       = element_text(hjust = 0.5), 
+              legend.position  = 'bottom', 
+              legend.direction = 'vertical', 
+              legend.title     = element_blank())
 }
 
 top_coef <- function(object, fit){
@@ -156,9 +157,9 @@ plot_top_volcano <- function(object, fit, coef){
     summarydt <- summarize_fit(fdt(object), fit = fit)
     summarydt %<>% extract(coefficient == coef)  # nup favours intercept !
     
-    plot_volcano(object, coef = coef) + 
+    plot_volcano(object, fit = fit, coef = coef, ) + 
     guides(color = 'none') + 
-    ggtitle('Volcano') + 
+    ggtitle(sprintf('%s~%s', fit, coef)) + 
     #ggtitle(sprintf('%d down | %d up', summarydt$ndown, summarydt$nup)) +
     theme(plot.title = element_text(hjust = 0.5))
 }
