13e20658de301b710d877b9530245ae50acdf58d
Aditya Bhagwat <aditya.bhagwat@uni-marburg.de>
2023-11-14 14:36:58 +0100

    1_download : Improve download_data : return all for NULL input, hard-code `url`, soft-code `localdir`, soft-code `force`, dont use BiocFileCache

--- R/1_download.R
+++ R/1_download.R
@@ -25,8 +25,7 @@ AUTONOMICS_DATASETS <- c(CORE_DATASETS,
 
 #' Download autonomics example data
 #' 
-#' @param filename file name
-#' @param url web url
+#' @param filename  file name
 #' \itemize{
 #'     \item \href{https://pubmed.ncbi.nlm.nih.gov/26857143/}{
 #'                 Billing 2016: stemcell comparison:} E, EM, BM
@@ -39,8 +38,8 @@ AUTONOMICS_DATASETS <- c(CORE_DATASETS,
 #'     \item \href{https://pubmed.ncbi.nlm.nih.gov/30525282/}{
 #'                 Atkin 2018: hypoglycemia:} t0, t1, t2, t3
 #'     \itemize{
-#'         \item \code{'atkin18.somascan.adat'}
-#'         \item \code{'atkin18.metbolon.xlsx'}
+#'         \item \code{'atkin.somascan.adat'}
+#'         \item \code{'atkin.metabolon.xlsx'}
 #'     }
 #'     \item \href{https://pubmed.ncbi.nlm.nih.gov/29777783/}{
 #'                 Halama 2018: glutaminase inhibition: } 4 conc, 4 timepoints
@@ -61,18 +60,20 @@ AUTONOMICS_DATASETS <- c(CORE_DATASETS,
 #'         \item \code{'fukuda20.proteingroups.txt'}
 #'     }
 #' }
-#' @param verbose TRUE / FALSE
+#' @param localdir local dir to save file to
+#' @param verbose  TRUE / FALSE
+#' @param force    TRUE / FALSE
 #' @return local file path
 #' @examples
-#' # atkin18 - hypoglycemia - pubmed 30525282
-#'     download_data('atkin18.somascan.adat')          # somascan  intensities
-#'     download_data('atkin18.metabolon.xlsx')         # metabolon intensities
+#' # atkin 2018 - hypoglycemia - pubmed 30525282
+#'     download_data('atkin.somascan.adat')            # somascan  intensities
+#'     download_data('atkin.metabolon.xlsx')           # metabolon intensities
 #'
 #' # billing16 - stemcell characterization - pubmed 26857143
-#'     download_data('billing16.proteingroups.txt')  # proteingroup ratios
-#'     download_data('billing16.somascan.adat')      # somascan  intensities
-#'     download_data('billing16.rnacounts.txt')      # rnaseq    counts
-#'     download_data('billing16.bam.zip')            # rnaseq    alignments
+#'     download_data('billing16.proteingroups.txt')      # proteingroup ratios
+#'     download_data('billing16.somascan.adat')          # somascan  intensities
+#'     download_data('billing16.rnacounts.txt')          # rnaseq    counts
+#'     download_data('billing16.bam.zip')                # rnaseq    alignments
 #'
 #' # billing19 - stemcell differentiation - pubmed 31332097
 #'     # download_data('billing19.proteingroups.txt')    # proteingroup ratios
@@ -80,63 +81,33 @@ AUTONOMICS_DATASETS <- c(CORE_DATASETS,
 #'     # download_data('billing19.rnacounts.txt')        # rnaseq       counts
 #'
 #' # fukuda20 - heart regeneration - pubmed PXD016235
-#'     download_data('fukuda20.proteingroups.txt')     # proteingroup LFQ
+#'     download_data('fukuda20.proteingroups.txt')       # proteingroup LFQ
 #'
 #' # halama18 - glutaminase inhibition - pubmed 30525282
-#'     download_data('halama18.metabolon.xlsx')        # metabolon intensities
+#'     download_data('halama18.metabolon.xlsx')          # metabolon intensities
 #' @export
 download_data <- function(
-    filename, 
-    url = paste0("https://bitbucket.org/graumannlabtools/autonomics/downloads/",
-        filename),
-    verbose = TRUE
+    filename = NULL,
+    localdir = file.path(R_user_dir('autonomics', 'cache'), 'datasets'),
+    verbose  = TRUE, 
+    force    = FALSE
 ){
     . <- NULL
+    if (is.null(filename))  return(AUTONOMICS_DATASETS)
     assert_is_subset(filename, AUTONOMICS_DATASETS)
-    bfc <- .get_cache()
-    rid <- BiocFileCache::bfcquery(bfc, filename, "rname")$rid
-    if (!length(rid)) {
+    dir.create(localdir, recursive = TRUE, showWarnings = FALSE)
+    filepath <- file.path(localdir, filename)
+    if (force | !file.exists(filepath)) {
         if(verbose) message( "Downloading ", filename)
-        rid <- names(BiocFileCache::bfcadd(bfc, filename, url ))
-    }
-    #if (!isFALSE(bfcneedsupdate(bfc, rid)))
-    #bfcdownload(bfc, rid)
-    filepath <- BiocFileCache::bfcrpath(bfc, rids = rid)
-    if (tools::file_ext(filename)=='zip'){
+        url <- "https://bitbucket.org/graumannlabtools/autonomics/downloads"
+        url %<>% paste0('/', filename)
+        download.file(url, filepath, mode = 'wb')  # wb needed to avoid
+    }                                              # corrupt excel files !
+    if (file_ext(filename)=='zip'){
         if (verbose)  message('unzip')
-        utils::unzip(filepath, exdir = substr(filepath, 1, nchar(filepath)-4))
+        unzip(filepath, exdir = substr(filepath, 1, nchar(filepath)-4))
         filepath %<>% substr(1, nchar(.)-4)
     }
     filepath
 }
 
-.get_cache <- function(){
-    cache <- rappdirs::user_cache_dir(appname="autonomics")
-    BiocFileCache::BiocFileCache(cache)
-}
-
-download_data_old <- function(
-    file, localdir = '~/autonomicscache/datasets', unzip = TRUE, ntries = 3
-){
-    assert_is_subset(file, AUTONOMICS_DATASETS)
-    . <- NULL
-    bitbucket <- 'https://bitbucket.org/graumannlabtools/autonomics/downloads'
-    localdir  %<>% paste(vapply(
-        stri_split_fixed(file, '.'), extract, character(1), 1), sep = '/')
-    dir.create(localdir, showWarnings = FALSE, recursive = TRUE)
-    localfile <- paste0(localdir,  '/', file)
-    if (!file.exists(localfile)){
-        bitbucketfile <- paste0(bitbucket, '/', file)
-        while (ntries > 0){  # bioconductor.org/developers/how-to/web-query
-            result <- tryCatch(
-                        download.file(bitbucketfile, localfile, mode = 'wb'), 
-                        error = identity)
-            if (!inherits(result, 'error'))  break
-            ntries %<>% magrittr::subtract(1) }
-        assertive.numbers::assert_all_are_not_equal_to(ntries, 0) }
-    if (file_ext(file) == 'zip'){
-        localdir <- file_path_sans_ext(localfile)
-        if (!dir.exists(localdir)) unzip(localfile, exdir = localdir)
-        localfile %<>% substr(1, nchar(.)-4) }
-    return(invisible(localfile))
-}
