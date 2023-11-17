783adaabdd0cf260ed33021879a138ce6bca0756
Aditya Bhagwat <aditya.bhagwat@uni-marburg.de>
2023-11-17 11:57:10 +0100

    3_read_omics : `add_subgroup` : hardcode 'subgroup' (subgroupvar renamed) and 'replicate', fill with 'group0' when no subgroup values given

--- R/3_read_omics.R
+++ R/3_read_omics.R
@@ -587,26 +587,29 @@ merge_ffile <- function(
 }
 
 add_subgroup <- function(
-    object, subgroupvar = 'subgroup', replicatevar = 'replicate', verbose=TRUE
+    object, subgroupvar = 'subgroup', verbose = TRUE
 ){
-    if (!has_some_svalues(object, subgroupvar)){
+    # Rename
+    object$subgroup <- object[[subgroupvar]]
+    object[[subgroupvar]] <- NULL
+
+    # Infer from sampleid if no subgroups defined yet
+    if (!has_some_svalues(object, 'subgroup')){
         x <- object$sample_id
         sep <- guess_sep(x)
-        if (sep=='NOSEP'){
-            subgroupvar <- NULL
-        } else {
-            if (is.null(subgroupvar))  subgroupvar <- 'subgroup'
+        if (sep!='NOSEP'){
             nfactor <- nfactors(x, sep)
-            if (verbose) message('\t\tInfer subgroup from sample_ids')
-            object[[subgroupvar]]  <- split_extract(x, seq_len(nfactor-1), sep)
-            object[[replicatevar]] <- split_extract(x, nfactor, sep)
+            if (verbose)  message('\t\tInfer subgroup from sample_ids')
+            object$subgroup  <- split_extract_fixed(x, sep, seq_len(nfactor-1))
+            object$replicate <- split_extract_fixed(x, sep, nfactor)
+        } else {
+            object$subgroup <- 'group0'
         }
     }
-    if (!is.null(subgroupvar)){
-        object[[subgroupvar]] %<>% factor()
-        levels(object[[subgroupvar]]) %<>% make.names() 
-        # otherwise issue in limma (fixable?)
-    }
+    
+    # factorify/validify
+    object$subgroup %<>% factor()
+    levels(object$subgroup) %<>% make.names()  # otherwise limma issue!
     object
 }
 
